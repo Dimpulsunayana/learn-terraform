f3989611e7db1e1fc727024a27ecf6866941fb1e
windows 10 <dimpulsunayana205@gmail.com>
2023-02-06 22:05:50 +0530

    Terraform sample

--- vars/vars.tf
+++ vars/vars.tf
@@ -1,22 +1,59 @@
-variable "sample" {
-  default = 10
-}
-output "demo" {
-  value=var.sample
+#variable "sample" {
+#  default = 10
+#}
+#output "demo" {
+#  value=var.sample
+#}
+#
+#variable "sample1" {
+#  default = "dimmi"
+#}
+#output "demo1" {
+#  value=var.sample1
+#}
+#
+#variable "sample2" {
+#  default = true
+#}
+#
+#output "demo2" {
+#  value=var.sample2
+#}
+
+#default variable type
+variable "sam" {
+  default =10
 }
 
-variable "sample1" {
-  default = "dimmi"
-}
-output "demo1" {
-  value=var.sample1
+output "dem" {
+  value=var.sam
 }
 
-variable "sample2" {
-  default = true
+#list variable type
+
+variable "sam1" {
+  default=[
+    10,
+    "sunnu",
+    100,
+    "pandu"
+    ]
 }
 
-output "demo2" {
-  value=var.sample2
+output "dem1" {
+  value=var.sam1
+}
+
+#map variable type
+
+variable "dem2" {
+  default={
+    name="dimpul",
+    num=10
+  }
+}
+
+output "dem2" {
+  value=var.sam2
 }
 
