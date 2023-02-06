9c8af18c543ad0630cc16683f578144e52c8610d
windows 10 <dimpulsunayana205@gmail.com>
2023-02-06 21:54:53 +0530

    Terraform sample

--- vars/vars.tf
+++ vars/vars.tf
@@ -1,7 +1,22 @@
 variable "sample" {
   default = 10
 }
-
 output "demo" {
   value=var.sample
 }
+
+variable "sample1" {
+  default = "dimmi"
+}
+output "demo1" {
+  value=var.sample1
+}
+
+variable "sample2" {
+  default = true
+}
+
+output "demo2" {
+  value=var.sample2
+}
+
