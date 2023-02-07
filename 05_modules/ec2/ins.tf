a194016af5dda8b9eaa6334b7a37bad07356b46e
windows 10 <dimpulsunayana205@gmail.com>
2023-02-07 22:36:19 +0530

    updated user repo

--- 05_modules/ec2/ins.tf
+++ 05_modules/ec2/ins.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = var.security_id
+  vpc_security_group_ids = [var.security_id]
 
   tags = {
     Name = "test-centos8"
