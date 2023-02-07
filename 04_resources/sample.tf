389b5ee9e36dd11534cb4cf56b285af82c5385d0
windows 10 <dimpulsunayana205@gmail.com>
2023-02-07 22:12:38 +0530

    updated user repo

--- 04_resources/sample.tf
+++ 04_resources/sample.tf
@@ -11,6 +11,7 @@ data "aws_ami" "example" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "test-centos8"
@@ -24,3 +25,29 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [0.0.0.0/0]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
