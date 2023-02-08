d5eff24912add5706d073740432e89075e024617
windows 10 <dimpulsunayana205@gmail.com>
2023-02-08 21:27:34 +0530

    updated user repo

---
+++ 06_loops/count/main.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  count = 1
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "test-centos8"
+  }
+}
+
+output "out" {
+  value = aws_instance.web.public_ip
+}
