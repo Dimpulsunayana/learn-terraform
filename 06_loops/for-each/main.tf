d6fc3eb71880eeaba71180d419acbac363ec3513
windows 10 <dimpulsunayana205@gmail.com>
2023-02-08 22:13:27 +0530

    updated user repo

---
+++ 06_loops/for-each/main.tf
@@ -0,0 +1,34 @@
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+resource "aws_instance" "web" {
+  for_each = var.demo
+  ami           = data.aws_ami.example.id
+  instance_type = each.value.instance_type
+
+  tags = {
+    Name = each.value.name
+  }
+}
+
+output "out" {
+  value = {
+    for k, v in aws_instance.web : k => v.public_ip
+  }
+}
+
+variable "demo" {
+  default = {
+    cart = {
+      name="cart"
+      instance_type="t3.small"
+    }
+    catalogue = {
+      name="catalogue"
+      instance_type="t3.micro"
+    }
+  }
+}
