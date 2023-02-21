bff7bc62cc894780fde0d2062b65929ff82f28bf
windows 10 <dimpulsunayana205@gmail.com>
2023-02-21 09:20:54 +0530

    updated user repo

--- 04_resources/sample.tf
+++ 04_resources/sample.tf
@@ -8,15 +8,15 @@ data "aws_ami" "example" {
   owners      = ["973714476881"]
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.example.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-
-#  tags = {
-#    Name = "test-centos8"
-#  }
-}
+#resource "aws_instance" "web" {
+#  ami           = data.aws_ami.example.id
+#  instance_type = "t3.micro"
+#  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+#
+##  tags = {
+##    Name = "test-centos8"
+##  }
+#}
 
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
@@ -27,36 +27,36 @@ resource "aws_s3_bucket" "b" {
 #  }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-dimpu"
-    key    = "04_resources/terraform-tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-dimpu"
+#    key    = "04_resources/terraform-tfstate"
+#    region = "us-east-1"
+#  }
+#}
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#resource "aws_security_group" "allow_tls" {
+#  name        = "allow_tls"
+#  description = "Allow TLS inbound traffic"
+#
+#  ingress {
+#    description      = "TLS from VPC"
+#    from_port        = 22
+#    to_port          = 22
+#    protocol         = "tcp"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#}
 
