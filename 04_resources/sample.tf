a78e1cf39bcde0739ae95c130ef6701a0e994d45
windows 10 <dimpulsunayana205@gmail.com>
2023-02-21 09:15:45 +0530

    updated user repo

--- 04_resources/sample.tf
+++ 04_resources/sample.tf
@@ -13,9 +13,9 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
-  tags = {
-    Name = "test-centos8"
-  }
+#  tags = {
+#    Name = "test-centos8"
+#  }
 }
 
 terraform {
