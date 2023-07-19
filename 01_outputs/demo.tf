2bf18380d84206c3d958c124d17140f9561720ba
windows 10 <dimpulsunayana205@gmail.com>
2023-07-19 18:58:42 +0530

    updated user repo

--- 01_outputs/demo.tf
+++ 01_outputs/demo.tf
@@ -1,3 +1,13 @@
 output "demo" {
   value = "Hello dimpukitty"
 }
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "terraform-dimpu"
+
+  #  tags = {
+  #    Name        = "terraform-dimpu"
+  #    #Environment = "Dev"
+  #  }
+}
