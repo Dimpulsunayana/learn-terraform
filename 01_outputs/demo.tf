a184056462f731891e51d8fc91690c5c509b0ad1
windows 10 <dimpulsunayana205@gmail.com>
2023-07-20 12:24:35 +0530

    updated user repo

--- 01_outputs/demo.tf
+++ 01_outputs/demo.tf
@@ -3,11 +3,26 @@ output "demo" {
 }
 
 
-resource "aws_s3_bucket" "b" {
-  bucket = "terraform-sunnu"
+#resource "aws_s3_bucket" "b" {
+#  bucket = "terraform-sunnu"
+#
+#  #  tags = {
+#  #    Name        = "terraform-dimpu"
+#  #    #Environment = "Dev"
+#  #  }
+#}
 
-  #  tags = {
-  #    Name        = "terraform-dimpu"
-  #    #Environment = "Dev"
-  #  }
+data "aws_ami" "example" {
+  most_recent = true
+ # name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["ami-053b0d53c279acc90"]
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.example.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
