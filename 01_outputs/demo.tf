9a80376dfb2cec686dbd69640f696cdee733608c
windows 10 <dimpulsunayana205@gmail.com>
2023-07-20 17:05:05 +0530

    updated user repo

--- 01_outputs/demo.tf
+++ 01_outputs/demo.tf
@@ -16,17 +16,17 @@ resource "aws_s3_bucket" "bamhu" {
   #  }
 }
 
-#data "aws_ami" "example" {
-#  most_recent = true
-# name_regex  = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516"
-#  owners      = ["099720109477"]
-#}
-#
-#resource "aws_instance" "web" {
-#  ami           = "${data.aws_ami.example.id}"
-#  instance_type = var.instance_type
-#
-#  tags = {
-#    Name = var.instance_name
-#  }
-#}
+data "aws_ami" "example" {
+  most_recent = true
+ name_regex  = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230516"
+  owners      = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.example.id}"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "dimpul"
+  }
+}
