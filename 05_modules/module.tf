3132e91aa4448882bb72e9fb227f12a2d7f490a3
windows 10 <dimpulsunayana205@gmail.com>
2023-07-21 12:50:48 +0530

    updated user repo

--- 05_modules/module.tf
+++ 05_modules/module.tf
@@ -7,6 +7,10 @@ module "sg" {
   source = "./sg"
 }
 
+module "s3" {
+  source = "./s3bucket"
+}
+
 provider "aws" {
   region = "us-east-1"
 }
---
+++ 05_modules/s3bucket/main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "bamhu" {
+  bucket = "terraform-dimpu"
+  #region = "ap-south-1"
+
+  #  tags = {
+  #    Name        = "terraform-dimpu"
+  #    #Environment = "Dev"
+  #  }
+}
